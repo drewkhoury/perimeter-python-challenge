9af312f5b632315533ba59d9b748e66762c6f3f1
Andrew Khoury <akhoury@odecee.com.au>
2014-07-29 21:35:17 +1000

    Fixed issue with shebang which had a typo (extra t in python)
    "env: pytthon: No such file or directory"

--- perimeter.py
+++ perimeter.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env pytthon
+#!/usr/bin/env python
 
 """
 read a list of points from a CSV file and print out the length of the perimiter of the shape that is formed by joining the points in their listed order
